f29dc987c9a98478899aca2fa2308349ebadd5c3
Alexandre Boin <36530710+alexboin@users.noreply.github.com>
2019-01-25 20:57:51 +0100

    feat(reader): add initial file reader

---
+++ game/util/reader.py
@@ -0,0 +1,20 @@
+import game.main.config as Config
+
+import json
+
+
+def loadConfig():
+	configFile = json.load(open(Config.CONFIG_PATH))['config']
+
+	Config.language = configFile['general']['language']
+
+	Config.width = configFile['window']['width']
+	Config.height = configFile['window']['height']
+	Config.ratio = Config.width / Config.height
+	Config.fullscreen = configFile['window']['fullscreen']
+	Config.limFrameRate = configFile['window']['framerate']
+
+	Config.musicVolume = configFile['audio']['music']/100
+	Config.soundsVolume = configFile['audio']['sounds']/100
+
+	Config.inputs = json.load(open(Config.INPUTS_PATH))["inputs"]
