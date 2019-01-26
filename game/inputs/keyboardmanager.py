1014fa2c64555807d901a4970c463848202fc0a7
MightyCode <bazinmaxence@free.fr>
2019-01-26 11:15:16 +0100

    style(keyboardManager): add the file description comment and a todo comment

--- game/inputs/keyboardmanager.py
+++ game/inputs/keyboardmanager.py
@@ -1,5 +1,10 @@
+# Manages key states
+
 from game.main import window
 import glfw
 
+
 def getKey(key):
 	return glfw.get_key(window.window, key)
+
+# TODO: create the keyboardManager's functions keyPressed and keyReleased
