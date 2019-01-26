b2cf2935eb7bf041ccf2d86bbc916b0fbb7f7ce2
MightyCode <bazinmaxence@free.fr>
2019-01-26 19:27:41 +0100

    refactor(camera-reader) rewrite the import of config

--- game/util/reader.py
+++ game/util/reader.py
@@ -1,6 +1,6 @@
 # This file loads the config and input files
 
-import game.main.config as Config
+from game.main.config import Config
 
 import json
 
@@ -16,7 +16,7 @@ def loadConfig():
 	Config.fullScreen = configFile['window']['fullscreen']
 	Config.limFrameRate = configFile['window']['framerate']
 
-	Config.musicVolume = configFile['audio']['music']/100
-	Config.soundsVolume = configFile['audio']['sounds']/100
+	Config.musicVolume = configFile['audio']['music'] / 100
+	Config.soundsVolume = configFile['audio']['sounds'] / 100
 
 	Config.inputs = json.load(open(Config.INPUTS_PATH))["inputs"]
