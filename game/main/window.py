17f8f2dd78966f9718fb277a3fbdfb2dafd173c6
MightyCode <bazinmaxence@free.fr>
2019-01-27 00:12:27 +0100

    refactor(window): place the window's code on a static class

--- game/main/window.py
+++ game/main/window.py
@@ -1,9 +1,11 @@
 # Game's initialisation : get the config, create the window, create gameManager, and init openGL / glfw
 
-from game.main.config import Config
 import time
+
 import OpenGL.GL as gl
 import glfw
+
+from game.main.config import Config
 
 SECOND = 1000000000
 TPS = 60.0
@@ -12,94 +14,98 @@ beginTime = 0
 ratio = 0
 
 
-def init():
-    Config.load()
-    beginTime = time.time()
+class Window:
+	window = None
 
-    # Initializing GLFW
-    if not glfw.init():
-        exit()
+	@staticmethod
+	def init():
+		Config.load()
+		beginTime = time.time()
 
-    create()  # the window
+		# Initializing GLFW
+		if not glfw.init():
+			exit()
 
-    from game.screen import gamemanager as gm
-    global gameManager
-    gameManager = gm.GameManager()
+		Window.create()  # the window
 
+		from game.screen import gamemanager as gm
+		global gameManager
+		gameManager = gm.GameManager()
 
-def create():
-    global window
+	@staticmethod
+	def create():
+		glfw.default_window_hints()
+		glfw.window_hint(glfw.RESIZABLE, 0)  # 0 = false
 
-    glfw.default_window_hints()
-    glfw.window_hint(glfw.RESIZABLE, 0)  # 0 = false
+		Window.window = glfw.create_window(Config.values["window"]["width"], Config.values["window"]["height"],
+									"Coop Dungeon Adventure", None, None)
 
-    window = glfw.create_window(Config.values["window"]["width"], Config.values["window"]["height"], "Coop Dungeon Adventure", None, None)
+		if not Window.window:
+			glfw.terminate()
+			exit()
 
-    if not window:
-        glfw.terminate()
-        exit()
+		glfw.make_context_current(Window.window)
 
-    glfw.make_context_current(window)
+		if Config.values["window"]["limFrameRate"]:
+			glfw.swap_interval(1)
+		else:
+			glfw.swap_interval(0)
 
-    if Config.values["window"]["limFrameRate"]:
-        glfw.swap_interval(1)
-    else:
-        glfw.swap_interval(0)
+		gl.glViewport(0, 0, Config.values["window"]["width"], Config.values["window"]["height"])
 
-    gl.glViewport(0, 0, Config.values["window"]["width"], Config.values["window"]["height"])
+		gl.glEnable(gl.GL_TEXTURE_2D)
+		gl.glActiveTexture(gl.GL_TEXTURE0)
+		gl.glEnable(gl.GL_BLEND)
+		gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+		gl.glMatrixMode(gl.GL_PROJECTION)
+		gl.glMatrixMode(gl.GL_MODELVIEW)
+		gl.glEnable(gl.GL_DEPTH_TEST)
+		# Enable Anti-aliasing
+		gl.glEnable(gl.GL_MULTISAMPLE)
 
-    gl.glEnable(gl.GL_TEXTURE_2D)
-    gl.glActiveTexture(gl.GL_TEXTURE0)
-    gl.glEnable(gl.GL_BLEND)
-    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-    gl.glMatrixMode(gl.GL_PROJECTION)
-    gl.glMatrixMode(gl.GL_MODELVIEW)
-    gl.glEnable(gl.GL_DEPTH_TEST)
-    # Enable Anti-aliasing
-    gl.glEnable(gl.GL_MULTISAMPLE)
+	@staticmethod
+	def run():
+		Window.loop()
+		Window.exit()
 
+	@staticmethod
+	def loop():
+		gl.glClearColor(0.05, 0.05, 0.05, 1.0)  # Black color
 
-def run():
-    loop()
-    exit()
+		ticks = 0
+		frames = 0
 
+		frameTime = time.time_ns()
+		secondTime = time.time_ns()
 
-def loop():
-    gl.glClearColor(0.05, 0.05, 0.05, 1.0)  # Black color
+		lag = 0.0
+		while not glfw.window_should_close(Window.window):
+			lag += time.time_ns() - frameTime
+			frameTime = time.time_ns()
 
-    ticks = 0
-    frames = 0
+			if time.time_ns() - secondTime >= SECOND:
+				if Config.debug:
+					glfw.set_window_title(Window.window,
+										  "Coop Dungeon Adventure | FPS:" + str(frames) + "; TPS:" + str(ticks))
+				ticks = 0
+				frames = 0
+				secondTime = time.time_ns()
 
-    frameTime = time.time_ns()
-    secondTime = time.time_ns()
+			while lag >= TICK_TIME:
+				gameManager.update()
 
-    lag = 0.0
-    while not glfw.window_should_close(window):
-        lag += time.time_ns() - frameTime
-        frameTime = time.time_ns()
+				lag -= TICK_TIME
+				ticks += 1
 
-        if time.time_ns() - secondTime >= SECOND:
-            if Config.debug:
-                glfw.set_window_title(window, "Coop Dungeon Adventure | FPS:" + str(frames) + "; TPS:" + str(ticks))
-            ticks = 0
-            frames = 0
-            secondTime = time.time_ns()
+			gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
-        while lag >= TICK_TIME:
-            gameManager.update()
+			gameManager.display()
+			frames += 1
 
-            lag -= TICK_TIME
-            ticks += 1
+			glfw.swap_buffers(Window.window)
+			glfw.poll_events()
 
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-
-        gameManager.display()
-        frames += 1
-
-        glfw.swap_buffers(window)
-        glfw.poll_events()
-
-
-def exit():
-    gameManager.unload()
-    glfw.terminate()
+	@staticmethod
+	def exit():
+		gameManager.unload()
+		glfw.terminate()
