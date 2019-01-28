56f085851aa1c2d2ae42f86b3410d37f8433f61a
MightyCode <bazinmaxence@free.fr>
2019-01-28 12:41:31 +0100

    feat(entities): add a basic system which contains and manages entities

---
+++ game/game/entitymanager.py
@@ -0,0 +1,44 @@
+from game.game import entity
+
+class EntityManager:
+
+	PLAYER_1 = 0
+	PLAYER_2 = 0
+
+	entities = []
+
+	@staticmethod
+	def init():
+		pass
+
+	@staticmethod
+	def addNew(args):
+		EntityManager.entities.append(entity.Entity(args))
+
+	@staticmethod
+	def add(entity):
+		EntityManager.entities.append(entity)
+
+	@staticmethod
+	def update():
+		for e in EntityManager.entities:
+			e.update()
+
+	@staticmethod
+	def display():
+		for e in EntityManager.entities:
+			e.display()
+
+	@staticmethod
+	def clear():
+		size = len(EntityManager.entities)
+		if  size > 2:
+			for i in range(3, size):
+				EntityManager.entities.remove(3)
+
+	@staticmethod
+	def unload():
+		print("\nUnload EntityManager :")
+		for e in EntityManager.entities:
+			e.unload()
+		print("\n")
--- game/screen/screens/gamescreen.py
+++ game/screen/screens/gamescreen.py
@@ -3,21 +3,70 @@
 from game.screen.screens import screen
 from game.render.texture import texture
 from game.render.shape import shape
-from game.render.shader import gluniforms as glU
-from game.screen import gamemanager as gameManager
-from game.inputs import keyboardmanager as key
+from game.screen.gamemanager import GameManager
 from game.inputs.inputmanager import InputManager as im
+from game.game.entitymanager import EntityManager as em
+from game.render.shader.shadermanager import ShaderManager as sm
+from game.game import player as pl
 
 import pyrr
-import glfw
-
+import time
 
 class GameScreen(screen.Screen):
 
 	def __init__(self):
 		super(GameScreen, self).__init__()
-		width = 18
-		height = 12
+		self.shape = None
+		self.tex = None
+
+		em.init()
+
+		player = pl.Player(["Player", [0,0], [1,1], "perso.png"])
+		em.add(player)
+
+		self.shape = shape.Shape(0)
+
+		self.modelMtx = pyrr.Matrix44.identity()
+		sm.updateLink(sm.TEXTURE, "model", self.modelMtx)
+		self.changeRoom()
+
+	def update(self):
+		# Receive and create data
+		serverData = GameManager.serverData
+		clientData = im.getState()
+
+		if im.inputPressed(im.RESET):
+			self.unload()
+			self.changeRoom()
+
+		em.update()
+
+		# Return data
+		clientData.append(time.time_ns())
+		GameManager.clientData = clientData
+
+	def display(self):
+		sm.updateLink(sm.TEXTURE, "model", self.modelMtx)
+		self.shape.applyShader()
+		self.shape.bind()
+
+		for i in range(0, len(self.tex)):
+			self.tex[i].bind()
+			self.shape.draw()
+
+		em.display()
+
+	def changeRoom(self):
+		# Clear the game before changing
+		em.clear()
+
+		# Load new room
+		from game.render import mapfunctions as mapFunctions
+		values = mapFunctions.createMap()
+		width = values[0][0]
+		height = values[0][1]
+
+		self.collision = values[2]
 
 		quad = [-width / 2, -height / 2, 0.0, 0.0, 0.0,
 				width / 2, -height / 2, 0.0, 1.0, 0.0,
@@ -26,35 +75,19 @@ class GameScreen(screen.Screen):
 
 		indices = [0, 1, 2,
 				   2, 3, 0]
-		self.shape = shape.Shape("texVert.glsl", "texFrag.glsl")
+
 		self.shape.setEbo(indices)
 		self.shape.setVertices(quad, [3, 2])
 
-		self.tex = texture.Texture("map1")
-		from game.render import mapfunctions as mapFunctions
-		self.tex.loadImage(mapFunctions.createMap(width, height, 32))
+		self.tex = [None] * len(values[3])
+		for i in range(0, len(values[3])):
+			self.tex[i] = texture.Texture("map1")
+			self.tex[i].loadImage(values[3][i])
 
-		self.tex.bind()
-		self.shape.shader.addLink("model")
-		self.modelMtx = pyrr.Matrix44.identity()
-		glU.glUniformv(self.shape.shader, "model", self.modelMtx)
-
-	def update(self):
-		if key.getKey(glfw.KEY_Z) == 1:
-			gameManager.GameManager.cam.addPos([0.0, 0.0, +0.01])
-
-		if key.getKey(glfw.KEY_P) == 1:
-			print(gameManager.GameManager.cam.camPos[2])
-
-		if im.input(im.GO_UP) and im.inputPressed(im.ITEM):
-			print("hey")
-
-	def display(self):
-		self.shape.applyShader()
-		self.shape.bind()
-		self.shape.view()
-		self.shape.draw()
+		em.entities[em.PLAYER_1].setPos(values[1])
 
 	def unload(self):
 		self.shape.unload()
-		self.tex.unload()
+		for i in range(0, len(self.tex)):
+			self.tex[i].unload()
+		em.unload()
