2e14e80e3759fcbfbb18580192a3d74f88df5a64
MightyCode <contact@bazin-maxence.tk>
2019-04-17 16:14:24 +0200

    fix(map4): add the collision with an interaction switch entity

--- game/game/map/mapmanager.py
+++ game/game/map/mapmanager.py
@@ -185,7 +185,7 @@ class MapManager:
 		MapManager.changeValues = ["null", "map0", 0]
 
 		# Force to load the first map with transition
-		MapManager.reserveChange("test", "map1", 0)
+		MapManager.reserveChange("test", "map4", 0)
 		MapManager.checkChangeMap()
 		MapManager.transitionPhase = 1
 		MapManager.update()
