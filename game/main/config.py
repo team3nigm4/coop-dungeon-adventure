35c7cc6967815cd0a925b60c00a043f64d80b0a8
Alexandre Boin <36530710+alexboin@users.noreply.github.com>
2019-05-11 15:46:49 +0200

    refactor(config): comment and improve the configuration class

--- game/main/config.py
+++ game/main/config.py
@@ -1,26 +1,30 @@
-# Check, load and save the config (path)
+# Class to create, check, load and save the differents configurations
 
 from sys import exit
+import json
 
 from game.util.logger import Logger
 
 class Config:
-	# Default value to prevent a mistake of configs's loading
-	server = []
-	CONFIG_PATH = "data/config/config.json"
-	INPUTS_PATH = "data/config/inputs.json"
+	
+	# Set the different paths of configuration files
+	DATA_FOLDER = "data"
+	MAIN_CONFIG_PATH = "data/config/config.json"
+	KEYBOARD_CONFIG_PATH = "data/config/inputs.json"
 	SERVER_CONFIG_PATH = "data/server.json"
 
 	ratio = 0
+	server = []
 	inputs = {}
 	values = {}
 
 	@staticmethod
 	def check():
+		
 		import os
 
-		# Verification of the existence of the "data" folder
-		path = "data"
+		# Check if the "data" folder exists (and create it if necessary)
+		path = Config.DATA_FOLDER
 		if not (os.path.exists(path)) and not (os.path.isdir(path)):
 			Logger.info("Config", "Creating '%s' folder..." % path)
 			try:
@@ -30,8 +34,8 @@ class Config:
 				Logger.error("Config", "Creation of the directory %s failed" % path)
 				exit()
 
-		# Verification of the existence of the "data/config" folder
-		path = "data/config"
+		# Check if the "data/config" folder exists (and create it if necessary) 
+		path = Config.DATA_FOLDER + "/config"
 		if not (os.path.exists(path)) and not (os.path.isdir(path)):
 			Logger.info("Config", "Creating '%s' folder..." % path)
 			try:
@@ -41,24 +45,25 @@ class Config:
 				Logger.error("Config", "Creation of the directory %s failed" % path)
 				exit()
 
-		# Verification of the existence of the user-specific config file
-		path = Config.CONFIG_PATH
+		# Check if the main configuration file exists (and create it if necessary)
+		path = Config.MAIN_CONFIG_PATH
 		if not (os.path.exists(path)):
 			Logger.info("Config", "Creating the user-specific configuration file...")
 			Config.createDefaultConfig()
 
-		# Verification of the existence of the user-specific key config file
-		path = Config.INPUTS_PATH
+		# Check if the keyboard configuration file exists (and create it if necessary)
+		path = Config.KEYBOARD_CONFIG_PATH
 		if not (os.path.exists(path)):
 			Logger.info("Config", "Creating the user-specific key configuration file...")
 			Config.createDefaultInputs()
 
 	@staticmethod
 	def checkServerConfig():
-		import os, sys
+		
+		import os
 
-		# Verification of the existence of the "data" folder
-		path = "data"
+		# Check if the "data" folder exists (and create it if necessary) 
+		path = Config.DATA_FOLDER
 		if not (os.path.exists(path)) and not (os.path.isdir(path)):
 			Logger.info("Config", "Creating '%s' folder..." % path)
 			try:
@@ -66,9 +71,9 @@ class Config:
 				Logger.success("Config", "Done !")
 			except OSError:
 				Logger.error("Config", "Creation of the directory %s failed" % path)
-				sys.exit()
+				exit()
 
-		# Verification of the existence of the server config file
+		# Check if the server configuration file exists (and create it if necessary) 
 		path = Config.SERVER_CONFIG_PATH
 		if not (os.path.exists(path)):
 			Logger.info("Config", "A configuration file needs to be created to connect to the server")
@@ -89,19 +94,21 @@ class Config:
 	@staticmethod
 	def createDefaultConfig(overwrite=True):
 
-		# We retrieve the user's screen resolution
-		width = 0
-		height = 0
+		# We retrieve the user's screen resolution using the GLFW library
+		width = 576
+		height = 384
 		import glfw
 		glfw.init()
 		vm = glfw.get_video_modes(glfw.get_primary_monitor())
 		nvm = len(vm) - 1
-		monitorResolution = [vm[nvm][0][0], vm[nvm][0][1]]
+		monitorRes = [vm[nvm][0][0], vm[nvm][0][1]]
+		
 		# We choose the highest resolution that can support the screen
-		baseResolution = [576, 384]  # The minimum game display resolution
-		for factor in range(1, 10):
-			if (baseResolution[0] * factor < monitorResolution[0] and baseResolution[1] * factor < monitorResolution[
-				1]): width = baseResolution[0] * factor; height = baseResolution[1] * factor
+		baseRes = [576, 384]  # The minimum game display resolution (18 tiles * 32 pixels and 12 tiles * 32 pixels)
+		for factor in range(1, 10): # Test differents zoom factors to find the maximum that can be used
+			if (baseRes[0] * factor < monitorRes[0] and baseRes[1] * factor < monitorRes[1]):
+				width = baseRes[0] * factor
+				height = baseRes[1] * factor
 
 		# We retrieve the user's locale
 		import locale
@@ -112,6 +119,7 @@ class Config:
 		for lang in languages:
 			if lang[0] == userLanguage: language = lang[0]
 
+		# We set the values for the configuration
 		Config.values = {
 			"general": {
 				"language": language,
@@ -129,12 +137,14 @@ class Config:
 			}
 		}
 
-		if overwrite:
-			Config.saveConfig()
+		# We overwrite the configuration if needed
+		if overwrite: Config.saveConfig()
 
+	
 	@staticmethod
 	def createDefaultInputs(overwrite=True):
-
+		
+		# We set default values for the keyboard configuration 
 		Config.inputs = {
 			"ECHAP": [[0, 256]],
 			"RESET": [[0, 82]],
@@ -154,46 +164,55 @@ class Config:
 			"ITEM2_1": [[0, 345]],
 		}
 
-		if overwrite:
-			Config.saveInputs()
+		# We overwrite the keyboard configuration if needed
+		if overwrite: Config.saveInputs()
 
 	@staticmethod
 	def createDefaultServerConfig(overwrite=True):
-		Config.server = {
-			"ip": input("[1/2] What is the IP or the domain name of the server ? "),
-			"port": input("[2/2] On which port runs the server (by default, 34141) ? ")
-		}
+		
+		if Config.yes(Logger.format("Config", "Do you want to use the default server configuration ? ")):	
+			# We set default values for the server configuration
+			Config.server = {
+				"ip": "127.0.0.1",
+				"port": 34141
+			}
+		else :	
+			# We ask for server configuration values
+			Config.server = {
+				"ip": input("[1/2] What is the IP or the domain name of the server ? "),
+				"port": input("[2/2] On which port runs the server (by default, 34141) ? ")
+			}
 
-		if overwrite:
-			Config.saveServerConfig()
+		# We overwrite the server configuration if needed
+		if overwrite: Config.saveServerConfig()
 
+	# Method to save the configuration into a file
 	@staticmethod
 	def saveConfig():
-		import json
 
 		ConfigToSave = {
 			"config": Config.values
 		}
 
-		with open(Config.CONFIG_PATH, 'w') as outfile:
+		with open(Config.MAIN_CONFIG_PATH, 'w') as outfile:
 			json.dump(ConfigToSave, outfile, indent="	")
 			Logger.success("Config", "Configuration file saved successfully !")
 
+	# Method to save the keyboard configuration into a file
 	@staticmethod
 	def saveInputs():
-		import json
 
 		InputsToSave = {
 			"inputs": Config.inputs
 		}
 
-		with open(Config.INPUTS_PATH, 'w') as outfile:
+		with open(Config.KEYBOARD_CONFIG_PATH, 'w') as outfile:
 			json.dump(InputsToSave, outfile, indent="	")
 			Logger.success("Config", "Key configuration file saved successfully !")
 
+	# Method to save the server configuration into a file
 	@staticmethod
 	def saveServerConfig():
-		import json
 
 		with open(Config.SERVER_CONFIG_PATH, 'w') as outfile:
 			json.dump(Config.server, outfile, indent="	")
@@ -201,33 +220,36 @@ class Config:
 
 	@staticmethod
 	def loadConfig():
-		import json
-		try:
-			configFile = json.load(open(Config.CONFIG_PATH))['config']
+		
+		try: # Try to load the configuration from file
+			configFile = json.load(open(Config.MAIN_CONFIG_PATH))['config']
 			Config.values = configFile
-		except json.decoder.JSONDecodeError:
+		except json.decoder.JSONDecodeError: # If it fails, ask for overwrite (or it will use a default configuration)
 			Config.createDefaultConfig(Config.yes(Logger.format("Config", "Failed to parse the config file ! Do you want to recreate it and delete the old one ?")))
 
 	@staticmethod
 	def loadInputs():
-		import json
-		try:
-			inputsFile = json.load(open(Config.INPUTS_PATH))['inputs']
+		
+		try: # Try to load the keyboard configuration from file
+			inputsFile = json.load(open(Config.KEYBOARD_CONFIG_PATH))['inputs']
 			Config.inputs = inputsFile
-		except json.decoder.JSONDecodeError:
+		except json.decoder.JSONDecodeError: # If it fails, ask for overwrite (or it will use a default keyboard configuration)
 			Config.createDefaultInputs(Config.yes(Logger.format("Config", "Failed to parse the key config file ! Do you want to recreate it and delete the old one ?")))
 
 	@staticmethod
 	def loadServerConfig():
-		import json
-		try:
+		
+		try: # Try to load the server configuration from file
 			Config.server = json.load(open(Config.SERVER_CONFIG_PATH))
-		except json.decoder.JSONDecodeError:
+		except json.decoder.JSONDecodeError: # If it fails, ask for overwrite (or it will use a default server configuration)
 			Config.createDefaultServerConfig(Config.yes(Logger.format("Config", "Failed to parse the server config file ! Do you want to recreate it and delete the old one ?")))
 
+	# Simple method that allow to ask questions to the user
 	@staticmethod
 	def yes(sentence):
+		
 		choice = input(sentence + " (Answer \"yes\") : ").lower()
+		
 		if choice == "yes":
 			return True
 		else:
